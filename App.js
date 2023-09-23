61dc0cc751a4f3a35a45d1cc004b918b469c9087
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-09-24 00:21:44 +0100

    Listing details screen

--- App.js
+++ App.js
@@ -22,20 +22,23 @@ import ViewImageScreen from './app/screens/ViewImageScreen';
 import AppText from './app/components/AppText/AppText';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 export default function App() {
   return (
-    <View style={{
-      backgroundColor: '#f8f4f4',
-      padding: 20,
-      paddingTop: 100
-    }}>
-      <Card
-        title="Red jacket for sale"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
+    // <View style={{
+    //   backgroundColor: '#f8f4f4',
+    //   padding: 20,
+    //   paddingTop: 100
+    // }}>
+    //   <Card
+    //     title="Red jacket for sale"
+    //     subTitle="$100"
+    //     image={require("./app/assets/jacket.jpg")}
+    //   />
+    // </View>
+
+    <ListingDetailsScreen />
   );
 }
 
