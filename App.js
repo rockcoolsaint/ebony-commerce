48269d6037eb6b892317977dcc514235fb454ce4
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-09-25 01:06:03 +0100

    picker modal and picker items component

--- App.js
+++ App.js
@@ -30,9 +30,16 @@ import { useState } from 'react';
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 
+const categories = [
+  { label: "Furniture", value: 1 },
+  { label: "Clothing", value: 2 },
+  { label: "Cameras", value: 3 },
+]
+
 export default function App() {
   // const [firstName, setFirstName] = useState('');
-  const [isNew, setIsNew] = useState(false);
+  // const [isNew, setIsNew] = useState(false);
+  const [category, setCategory] = useState(categories[0]);
 
   return (
     <Screen>
@@ -54,7 +61,10 @@ export default function App() {
 
       {/* <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} /> */}
 
-      <AppPicker icon="apps" placeholder={"Category"} />
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={item => setCategory(item)}
+        items={categories} icon="apps" placeholder={"Category"} />
       <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
