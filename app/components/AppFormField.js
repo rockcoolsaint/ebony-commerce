804c5ef65985700d40eb03302483d5f47a4017ee
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-09-28 13:14:59 +0100

    formik input components refactoring using formik context

---
+++ app/components/AppFormField.js
@@ -0,0 +1,20 @@
+import { useFormikContext } from 'formik';
+import React from 'react';
+import AppTextInput from './AppTextInput';
+import ErrorMessage from './ErrorMessage';
+
+function AppFormField({name, ...rest}) {
+  const {setFieldTouched, handleChange, errors, touched} = useFormikContext();
+  return (
+    <>
+      <AppTextInput
+        onBlur={() => setFieldTouched(name)}
+        onChangeText={handleChange(name)}
+        {...rest}
+      />
+      <ErrorMessage error={errors[name]} visible={touched[name]} />
+    </>
+  );
+}
+
+export default AppFormField;
