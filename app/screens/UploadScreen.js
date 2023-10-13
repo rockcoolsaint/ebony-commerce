8f9f3e34ce80cde8e66a6f51794b12627e9c5937
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-10-13 22:28:20 +0100

    showing the done animation

--- app/screens/UploadScreen.js
+++ app/screens/UploadScreen.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Modal, StyleSheet, View } from 'react-native';
 import * as Progress from 'react-native-progress';
+import AnimatedLottieView from 'lottie-react-native';
 
 import Text from '../components/AppText';
+import colors from '../config/colors';
 
-function UploadScreen({progress = 0, visible = false}) {
+function UploadScreen({onDone, progress = 0, visible = false}) {
   return (
     <Modal visible={visible}>
       <View style={styles.container}>
-        <Progress.Bar color={colors.primary} progress={progress} width={200} />
+        {progress < 1 ? (
+          <Progress.Bar color={colors.primary} progress={progress} width={200} />
+        ) : (
+          <AnimatedLottieView
+            autoPlay
+            loop={false}
+            source={require('../assets/animations/done.json')}
+            style={styles.animation}
+            onAnimationFinish={onDone}
+          />
+        )}
       </View>
     </Modal>
   );
@@ -19,6 +31,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     justifyContent: 'center'
+  },
+  animation: {
+    width: 150
   }
 })
 
