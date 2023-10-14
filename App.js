3e334cfba4ba3d84babbc64660ebf8ae05b2d5c3
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-10-14 21:20:43 +0100

    showing an offline notice

--- App.js
+++ App.js
@@ -44,6 +44,7 @@ import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import OfflineNotice from './app/components/OfflineNotice';
 
 // const categories = [
 //   { label: "Furniture", value: 1 },
@@ -140,6 +141,7 @@ export default function App() {
 
   return (
     <GestureHandlerRootView style={{flex: 1}}>
+      <OfflineNotice />
       <NavigationContainer theme={navigationTheme}>
         {/* <StackNavigator /> */}
         {/* <TabNavigator/> */}
