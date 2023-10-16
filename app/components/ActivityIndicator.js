c09bac4e4e668e963acd8503ccb437b2ba60c0cf
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-10-16 23:44:09 +0100

    ActivityIndicator overlay

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,14 +1,30 @@
 import AnimatedLottieView from 'lottie-react-native';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 
 function ActivityIndicator({ visible = false }) {
   if (!visible) return null;
 
-  return <AnimatedLottieView
-    autoPlay
-    loop
-    source={require('../assets/animations/loader.json')}
-  />;
+  return (
+    <View style={styles.overlay}>
+      <AnimatedLottieView
+        autoPlay
+        loop
+        source={require('../assets/animations/loader.json')}
+      />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  overlay: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    height: '100%',
+    opacity: 0.8,
+    width: '100%',
+    zIndex: 1
+  }
+})
 
 export default ActivityIndicator;
