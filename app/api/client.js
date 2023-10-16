b0c283c7ee9dbde3ae6983fb0c0250d9ec024877
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-10-16 22:07:44 +0100

    calling protected APIs

--- app/api/client.js
+++ app/api/client.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { create } from "apisauce";
 import cache from "../../utility/cache";
+import authStorage from "../auth/storage";
 
 // hosted url
 const apiClient = create({
@@ -11,6 +12,12 @@ const apiClient = create({
 // const apiClient = create({
 //   baseURL: 'http://192.168.8.159:9000/api',
 // });
+
+apiClient.addAsyncRequestTransform(async (request) => {
+  const authToken = await authStorage.getToken();
+  if (!authToken) return;
+  request.headers["x-auth-token"] = authToken;
+});
 
 const get = apiClient.get;
 apiClient.get = async (url, params, axiosConfig) => {
