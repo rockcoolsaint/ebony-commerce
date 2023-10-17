24f5ae2d23c811be88e3c1442a371263de21865e
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-10-17 23:25:32 +0100

    push notification setup

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -6,28 +6,51 @@ import FeedNavigator from "./FeedNavigator";
 import {MaterialCommunityIcons} from '@expo/vector-icons'
 import AccountNavigator from "./AccountNavigator";
 import NewListingButton from "./NewListingButton";
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import { useEffect } from "react";
 
 const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-  <Tab.Navigator screenOptions={{ headerShown: false }} >
-    <Tab.Screen name="Feed" component={FeedNavigator}
-      options={{
-        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='home' size={size} color={color} />
-      }} 
-    />
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen}
-      options={({ navigation }) => ({
-        tabBarButton: () => <NewListingButton onPress={() => navigation.navigate("ListingEdit")} />,
-        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='plus-circle' size={size} color={color} />
-      })}
-    />
-    <Tab.Screen name="Account" component={AccountNavigator}
-      options={{
-        tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='account' size={size} color={color} />
-      }}
-    />
-  </Tab.Navigator>
-)
+const AppNavigator = () => {
+  useEffect(() => {
+    registerForPushNotifications();
+  }, []);
+
+  const registerForPushNotifications = async () => {
+    try {
+      const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      if (!permission.granted) return;
+
+      const token = await Notifications.getExpoPushTokenAsync({
+        'projectId': "com.rockcoolsaint.donewith"
+      });
+      console.log(token);
+    } catch (error) {
+      console.log('Error getting a push token', error)
+    }
+  }
+
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }} >
+      <Tab.Screen name="Feed" component={FeedNavigator}
+        options={{
+          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='home' size={size} color={color} />
+        }}
+      />
+      <Tab.Screen name="ListingEdit" component={ListingEditScreen}
+        options={({ navigation }) => ({
+          tabBarButton: () => <NewListingButton onPress={() => navigation.navigate("ListingEdit")} />,
+          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='plus-circle' size={size} color={color} />
+        })}
+      />
+      <Tab.Screen name="Account" component={AccountNavigator}
+        options={{
+          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='account' size={size} color={color} />
+        }}
+      />
+    </Tab.Navigator>
+  )
+}
 
 export default AppNavigator;
