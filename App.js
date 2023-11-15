164fd2b6f0fb9d6a8033de54ffaf8d4f3a34d10d
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-11-15 23:45:30 +0100

    expopushtoken

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import Screen from './app/components/Screen';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 // import AppTextInput from './app/components/AppTextInput';
 // import AppPicker from './app/components/AppPicker';
 import LoginScreen from './app/screens/LoginScreen';
@@ -68,13 +68,45 @@ export default function App() {
     if (user) setUser(user);
   }
 
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        // await new Promise(resolve => setTimeout(resolve, 2000));
+        restoreUser();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [isReady]);
+
   if(!isReady)
-    return (
-      <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} onError={console.warn} />
-    );
+    return null;
+    // return (
+    //   <AppLoading startAsync={restoreUser} onFinish={() => setIsReady(true)} onError={console.warn} />
+    // );
 
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
+    <GestureHandlerRootView style={{flex: 1}} onLayout={onLayoutRootView} >
       <AuthContext.Provider value={{ user, setUser }}>
         <OfflineNotice />
         <NavigationContainer theme={navigationTheme}>
--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect, useRef } from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AccountScreen from "../screens/AccountScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
@@ -9,50 +9,154 @@ import AccountNavigator from "./AccountNavigator";
 import NewListingButton from "./NewListingButton";
 import * as Notifications from 'expo-notifications';
 import * as Permissions from 'expo-permissions';
-import { Constants } from "expo-constants";
+import Constants from "expo-constants";
 import expoPushTokensApi from "../api/expoPushTokens";
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  // useEffect(() => {
+  //   registerForPushNotifications();
+  // }, []);
+
+  // const registerForPushNotifications = async () => {
+  //   try {
+  //     const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  //     if (!permission.granted) return;
+
+  //     const token = await Notifications.getExpoPushTokenAsync({
+  //       projectId: Constants.expoConfig.extra.eas.projectId,
+  //     });
+  //     expoPushTokensApi.register(token);
+  //   } catch (error) {
+  //     console.log('Error getting a push token', error)
+  //   }
+  // }
+
   useEffect(() => {
-    registerForPushNotifications();
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+    // console.log("=====register push token======")
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
   }, []);
 
-  const registerForPushNotifications = async () => {
-    try {
-      const permission = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-      if (!permission.granted) return;
-
-      const token = await Notifications.getExpoPushTokenAsync({
-        projectId: Constants.expoConfig.extra.eas.projectId,
-      });
-      expoPushTokensApi.register(token);
-    } catch (error) {
-      console.log('Error getting a push token', error)
-    }
-  }
+  // async function registerForPushNotificationsAsync() {
+  //   let token;
+  
+  //   if (Platform.OS === 'android') {
+  //     await Notifications.setNotificationChannelAsync('default', {
+  //       name: 'default',
+  //       importance: Notifications.AndroidImportance.MAX,
+  //       vibrationPattern: [0, 250, 250, 250],
+  //       lightColor: '#FF231F7C',
+  //     });
+  //   }
+  
+  //   if (Device.isDevice) {
+  //     const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  //     let finalStatus = existingStatus;
+  //     if (existingStatus !== 'granted') {
+  //       const { status } = await Notifications.requestPermissionsAsync();
+  //       finalStatus = status;
+  //     }
+  //     if (finalStatus !== 'granted') {
+  //       alert('Failed to get push token for push notification!');
+  //       return;
+  //     }
+  //     // Learn more about projectId:
+  //     // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+  //     token = (await Notifications.getExpoPushTokenAsync({ projectId: Constants.expoConfig?.extra?.eas.projectId })).data;
+  //     console.log("token");
+  //     console.log(token);
+  //   } else {
+  //     alert('Must use physical device for Push Notifications');
+  //   }
+  
+  //   return token;
+  // }
 
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }} >
-      <Tab.Screen name="Feed" component={FeedNavigator}
-        options={{
-          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='home' size={size} color={color} />
-        }}
-      />
-      <Tab.Screen name="ListingEdit" component={ListingEditScreen}
-        options={({ navigation }) => ({
-          tabBarButton: () => <NewListingButton onPress={() => navigation.navigate("ListingEdit")} />,
-          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='plus-circle' size={size} color={color} />
-        })}
-      />
-      <Tab.Screen name="Account" component={AccountNavigator}
-        options={{
-          tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='account' size={size} color={color} />
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      {/* <Text>Your expo push token: {expoPushToken}</Text> */}
+      <Tab.Navigator screenOptions={{ headerShown: false }} >
+        <Tab.Screen name="Feed" component={FeedNavigator}
+          options={{
+            tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='home' size={size} color={color} />
+          }}
+        />
+        <Tab.Screen name="ListingEdit" component={ListingEditScreen}
+          options={({ navigation }) => ({
+            tabBarButton: () => <NewListingButton onPress={() => navigation.navigate("ListingEdit")} />,
+            tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='plus-circle' size={size} color={color} />
+          })}
+        />
+        <Tab.Screen name="Account" component={AccountNavigator}
+          options={{
+            tabBarIcon: ({size, color}) => <MaterialCommunityIcons name='account' size={size} color={color} />
+          }}
+        />
+      </Tab.Navigator>
+    </>
   )
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    // Learn more about projectId:
+    // https://docs.expo.dev/push-notifications/push-notifications-setup/#configure-projectid
+    token = (await Notifications.getExpoPushTokenAsync({ 'projectId': Constants.expoConfig?.extra?.eas.projectId })).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return token;
 }
 
 export default AppNavigator;
